ca7eb47dc9451179bd4337f4f4af0ce1efc8ad37
AHMED <hossainahmad76@gmail.com>
2023-11-05 21:42:47 +0600

    Task Manager App

--- lib/ui/screen/splash_screen.dart
+++ lib/ui/screen/splash_screen.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
+  State<SplashScreen> createState() => _SplashScreenState();
+}
 
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Stack(
+        children: [
+          SvgPicture.asset('assets/images/background.svg',width: double.infinity,height: double.infinity,),
+          Center(
+            child: SvgPicture.asset('assets/images/logo.svg'),
+          ),
+        ],
+      ),
     );
   }
 }
