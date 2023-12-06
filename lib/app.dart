4ff52e9475fbaf66150e729ede1df0f1f7b26ac9
AHMED <hossainahmad76@gmail.com>
2023-12-06 23:00:34 +0600

    Use GetX

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:task_manager/ui/screen/splash_screen.dart';
 
 //Theme Data
@@ -10,7 +11,7 @@ class TaskManagerApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       navigatorKey: navigationKey,
       home: const SplashScreen(),
       theme: ThemeData(
